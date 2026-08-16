ffdd482c7c5e512875c5366a33cd9a0dc66e6347
viktor-taraba <victortaraba@gmail.com>
2026-08-16 17:36:19 +0300

    report_send state as separate google sheets page instead of json

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ WORKSHEET_EXPENSE = "Витрати"
 WORKSHEET_INCOME = "Доходи"
 WORKSHEET_DELETED = "Deleted"
 WORKSHEET_CATEGORIES = "Categories"
+WORKSHEET_EMAIL_LOG = "Emails"
 COLUMN_ORDER = ["date", "category", "amount", "note", "submitted_at", "added_at", "updated_at", "device_info", "split_id", "split_info", "subcategory", "curr_amount", "exchange_rate", "currency"]
 DELETED_COLUMN_ORDER = ["date", "category", "amount", "note", "submitted_at", "added_at", "updated_at", "device_info", "deleted_at", "income_or_expense", "split_id", "split_info", "subcategory", "curr_amount", "exchange_rate", "currency"]
+EMAIL_LOG_COLUMN_ORDER = ["date", "sent_at"]
